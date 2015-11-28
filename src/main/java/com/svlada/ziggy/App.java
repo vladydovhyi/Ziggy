dee6140c8b0d17e50fc366a35a165795924b288a
vladydovhyi <vladyslav.dovhyi2@gmail.com>
2015-11-27 21:31:41 -0800

    Created maven project structure.

---
+++ src/main/java/com/svlada/ziggy/App.java
@@ -0,0 +1,10 @@
+package com.svlada.ziggy;
+
+import org.apache.log4j.Logger;
+
+public class App {
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(App.class);
+		logger.info("Test.");
+	}
+}
